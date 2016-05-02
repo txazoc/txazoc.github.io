66051be82b04518b03577789909846b6dc1cb5e5
txazoc <txazoc@163.com>
2016-05-02 23:01:57 +0800

    update

--- js/data.js
+++ js/data.js
@@ -2,4 +2,4 @@
 # data.js
 ---
 
-var Post = [{% for post in site.posts %}{'id': '{{ post.id }}', 'title': '{{ post.title }}', 'date': '{{ post.date | date: "%Y年%m月%d日" }}', 'tags': '{% for tag in post.tags %}{{ tag }} {% endfor %}', 'categories': '{{ post.categories[0] }}', 'url': '{{ post.url }}', 'content': '{{ post.content | strip_html | strip_newlines | truncate: 150 }}'}{% if post.previous != nil %},{% endif %}{% endfor %}];
+var Post = [{% for post in site.posts %}{"id":"{{ post.id }}","title":"{{ post.title }}","date":"{{ post.date | date: "%Y年%m月%d日" }}","tags":"{% for tag in post.tags %}{{ tag }} {% endfor %}","categories":"{{ post.categories[0] }}","url":"{{ post.url }}","content":"{{ post.content | strip_html | strip_newlines | truncate: 150 }}"}{% if post.previous != nil %},{% endif %}{% endfor %}];
