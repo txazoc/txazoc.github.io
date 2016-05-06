e622e0a0fdb83a91a69fe59d5db021e611485aee
txazoc <txazoc@163.com>
2016-05-07 00:27:53 +0800

    update

--- js/load.js
+++ js/load.js
@@ -1,8 +1,8 @@
 seajs.config({
     base: '/js',
     alias: {
-        'init': 'init.js?v=2016050608',
-        'data': 'data.js?v=20160505',
+        'init': 'init.js?v=20160507',
+        'data': 'data.js?v=20160507',
         'highlight': '/js/lib/highlight.js'
     }
 });
