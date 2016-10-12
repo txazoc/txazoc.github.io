a674471fe4dd3ed1d8373242d194e6144dbf761c
txazoc <txazoc@163.com>
2016-10-12 16:35:38 +0800

    update

--- _plugins/topic.rb
+++ _plugins/topic.rb
@@ -1,10 +1,31 @@
 #!/usr/bin/ruby -w
 # -*- coding: UTF-8 -*-
 
-module Topic
+require 'pathname'
+
+module Jekyll
   class TopicGenerator < Jekyll::Generator
+    include Convertible
+
+    attr_accessor :site, :content, :data
+
     def generate(site)
-      site.pages.each { |page| puts page.name }
+      @site = site
+      _base = File.dirname(File.dirname(Pathname.new(__FILE__).realpath))
+      _topic = {}
+      site.pages.each {
+          |page|
+        if page.path.match(/^topic\//)
+          read_yaml(File.join(_base, page.dir), page.name)
+          data['url'] = page.url
+
+          if !_topic.has_key?(data['module'])
+            _topic[data['module']] = Array.new
+          end
+
+          _topic[data['module']] << data
+        end
+      }
     end
   end
 end
