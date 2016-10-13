2f978fde68f6234b67c37945ad40897430c3ca2d
txazoc <txazoc@163.com>
2016-10-13 22:59:11 +0800

    update

--- script/cdn-refresh.py
+++ script/cdn-refresh.py
@@ -1,9 +1,32 @@
 import cdn
+import time
 
 def cdnRefresh(path):
     resp = cdn.request({'Action': 'PushObjectCache', 'ObjectPath': path})
     if resp.has_key('PushTaskId'):
-        print '[CDN Refresh]:\t' + resp['PushTaskId'] + '\t' + path
+        return resp['PushTaskId']
+    else:
+        print '[CDN Refresh]: Error ' + path
+        return 0
 
-cdnRefresh('http://image.txazo.com/js/init.js')
-cdnRefresh('http://image.txazo.com/js/load.js')
+def cdnRefreshQuery(path):
+    taskId = cdnRefresh('http://image.txazo.com/js/init.js')
+    if taskId > 0:
+        print '[CDN Refresh]: Refreshing ' + path + ' ...'
+        for i in range(1, 15):
+            resp = cdn.request({'Action': 'DescribeRefreshTasks', 'TaskId': str(taskId)})
+            if resp.has_key('Tasks'):
+                Status = resp['Tasks']['CDNTask'][0]['Status']
+                if Status == 'Complete':
+                    print '[CDN Refresh]: Complete --------------------------------------------------'
+                    break
+                else:
+                    time.sleep(2)
+
+cdnRefreshQuery('http://image.txazo.com/css/core.css')
+cdnRefreshQuery('http://image.txazo.com/css/input.min.css')
+cdnRefreshQuery('http://image.txazo.com/js/init.js')
+cdnRefreshQuery('http://image.txazo.com/js/load.js')
+cdnRefreshQuery('http://image.txazo.com/js/data.js')
+cdnRefreshQuery('http://image.txazo.com/js/topic.js')
+cdnRefreshQuery('http://image.txazo.com/js/lib/seajs-css.js')
