885e9965a409713de3a8962191a462ce83638563
txazoc <txazoc@163.com>
2016-10-13 17:56:50 +0800

    update

--- js/load.js
+++ js/load.js
@@ -1,10 +1,14 @@
+---
+# load.js
+---
+
 seajs.config({
     base: '/js',
     alias: {
-        'init': 'init.js',
-        'data': 'data.js',
-        'topic': 'topic.js',
-        'input-css': '../css/input.min.css'
+        'init': 'http://image.txazo.com/js/init.js',
+        'data': 'http://image.txazo.com/js/data.js',
+        'topic': 'http://image.txazo.com/js/topic.js',
+        'input-css': 'http://image.txazo.com/css/input.min.css'
     }
 });
 
