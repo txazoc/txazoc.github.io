939311bb2fe414b606c10e26e4b2e9a5fa305f2b
txazoc <txazoc@163.com>
2017-03-31 15:20:26 +0800

    auto commit

--- push.sh
+++ push.sh
@@ -8,14 +8,3 @@ python2 script/compress.py
 git add .
 git cm 'auto commit'
 git push origin master
-
-echo '--------------------------------------------------'
-echo 'sleep 5s waiting github pages to build ...'
-
-sleep 5
-
-echo '[CDN Refresh] begin --------------------------------------------------'
-
-python script/cdn-refresh.py
-
-echo '[CDN Refresh] end --------------------------------------------------'
